0097ff3ee81db533c6d10c10ad1c030735a79c6b
AyushSingh16 <ayushsingh888777@gmail.com>
2024-07-19 21:55:54 +0530

    updated user.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const {login, signup} = require("../controllers/auth");
 const {auth, isStudent, isAdmin} = require("../middlewares/auth");
 // const { model } = require("mongoose");
